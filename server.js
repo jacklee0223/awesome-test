e001a275049f4dc8817c0767989b63bbd6edce57
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-12-11 01:17:56 -0800

    Created User API, need to fix not posting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
-    User = require('./app/models/user')
+    User = require('./app/models/user'),
     port = process.env.PORT || 3000;
 
 // APP CONFIGURATION -----------------
@@ -31,8 +31,46 @@ app.get('/', function(req, res) {
   res.send('Welcome to the home page!');
 });
 
-// get an instance of the express router
-var apiRouter = express.Router();
+// ROUTES FOR OUR API
+//====================
+var apiRouter = express.Router();     // get an instance of the express Router
+
+// middleware to use for all requests
+apiRouter.use(function(req, res, next) {
+  // do logging
+  console.log('Somebody just came to our app!');
+  next(); // make sure we go to the next routes and don't stop here
+
+})
+
+// route middleware and first route are here
+
+// on routes that end in /users
+apiRouter.route('/users')
+
+  // create a user (accessed at POST http://localhost:3000/api/users)
+  .post(function(req, res) {
+
+    // create a new instance of the User model
+    var user = new User();
+
+    // set the users information (comes from the request)
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    // save the user and check for errors
+    user.save(function(err) {
+      if(err) {
+        // duplicate entry
+        if(err.code == 11000)
+          return res.json({ success: false, message: 'A user with that username already exists.' });
+        else
+          return res.send(err);
+      }
+        res.json({ message: 'User created!' });
+    });
+  })
 
 // test route to make sure everything is working
 // accessed at GET http://localhost:3000/api
